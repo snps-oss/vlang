bd4713765e5494ac6e390894c2ea9c55ed7c1722
Maurizio Vitale <mrz.vtl@gmail.com>
2014-03-12 16:55:44 -0400

    Beginning of CMake configuration for vlang

---
+++ utils/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git submodule update --init
+
+if [ ! -e llvm/tools/clang ]; then
+    ln -s clang llvm/tools/clang
+fi
+
+if [ ! -e llvm/tools/vlang ]; then
+    ln -s vlang llvm/tools/vlang
+fi
