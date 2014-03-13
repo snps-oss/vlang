077fb5f0736d95837c70fe82c36d76ff0cfead70
Maurizio Vitale <mrz.vtl@gmail.com>
2014-03-12 21:51:20 -0400

    proper symbolic links for submodule (and vlang proper). Everything is now in place, only nit is that now the llvm submodule looks modified in git. Maybe we should add that to .gitignore.

--- utils/setup.sh
+++ utils/setup.sh
@@ -2,10 +2,10 @@
 
 git submodule update --init
 
-#if [ ! -e llvm/tools/clang ]; then
-#    ln -s clang llvm/tools/clang
-#fi
+if [ ! -e $(pwd)/llvm/tools/clang ]; then
+    ln -s $(pwd)/clang $(pwd)/llvm/tools/clang
+fi
 
-#if [ ! -e llvm/tools/vlang ]; then
-#    ln -s vlang llvm/tools/vlang
-#fi
+if [ ! -e $(pwd)/llvm/tools/vlang ]; then
+    ln -s $(pwd)/vlang $(pwd)/llvm/tools/vlang
+fi
